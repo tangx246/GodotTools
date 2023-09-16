0c3d7d2e41922e07d7e3638c3632c7ac3734ede2
Yee Chien Tang <tangx246@umn.edu>
2023-09-16 10:17:54 -0700

    Initial commit

---
+++ addons/PlayerController/FPS/Player.gd
@@ -0,0 +1,39 @@
+extends CharacterBody3D
+
+@export var gravity : Vector3 = Vector3(0, -9.81, 0)
+@export var speed : float = 22
+@export var jump_height : float = 2.4
+@export var mouse_look_speed : float = 0.002
+@onready var camera : Camera3D = get_node("Camera3D")
+var jump_velocity_to_add : float = 0
+var mouse_movement : Vector2 = Vector2.ZERO
+
+func get_input() -> Vector3:
+	var input_dir = Input.get_vector("Strafe Left", "Strafe Right", "Move Forward", "Move Backward")
+	var velocity2 = (input_dir * speed)
+	return Vector3(velocity2.x, velocity.y, velocity2.y)
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event:InputEvent):
+	if is_on_floor() and event.is_action_pressed("Jump"):
+		jump_velocity_to_add = sqrt(2*jump_height*(-gravity.y))
+	if event is InputEventMouseMotion:
+		mouse_movement.x -= event.relative.x * mouse_look_speed
+		mouse_movement.y -= event.relative.y * mouse_look_speed
+
+func _physics_process(delta):
+	velocity = get_input()
+	velocity += gravity * delta
+	velocity += Vector3(0, jump_velocity_to_add, 0)
+	jump_velocity_to_add = 0
+	
+	velocity = global_transform.basis * velocity
+	
+	move_and_slide()
+	
+	rotate_y(mouse_movement.x)
+	mouse_movement.x = 0
+	camera.rotate_x(mouse_movement.y)
+	mouse_movement.y = 0
