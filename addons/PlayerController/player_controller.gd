aab46db8eab05a0173d259cf02f5072062914278
Yee Chien Tang <tangx246@umn.edu>
2023-09-16 11:39:15 -0700

    Automatically save InputMap

--- addons/PlayerController/player_controller.gd
+++ addons/PlayerController/player_controller.gd
@@ -4,7 +4,29 @@ extends EditorPlugin
 
 func _enter_tree():
 	add_custom_type("FPSController", "CharacterBody3D", preload("FPS/Player.gd"), preload("icon.svg"))
+	add_fps_controller_input_map()
 
 
 func _exit_tree():
 	remove_custom_type("FPSController")
+
+func add_fps_controller_input_map():
+	var input_to_events = {
+		"input/Move Forward": KEY_W,
+		"input/Move Backward": KEY_S,
+		"input/Strafe Left": KEY_A,
+		"input/Strafe Right": KEY_D,
+		"input/Jump": KEY_SPACE
+	}
+	
+	for input in input_to_events:
+		var input_event_key = InputEventKey.new()
+		input_event_key.physical_keycode = input_to_events[input]
+		if not ProjectSettings.has_setting(input):
+			ProjectSettings.set_setting(input,
+			{
+				"deadzone": 0.5,
+				"events": [input_event_key]
+			})
+
+	ProjectSettings.save()
