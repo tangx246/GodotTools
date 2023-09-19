19d9d0dd32a5bf41b172ed9aef44b46b4405a6a4
Yee Chien Tang <tangx246@umn.edu>
2023-09-19 15:04:19 -0700

    Updated logic for player controller

--- addons/PlayerController/Topdown/Player.gd
+++ addons/PlayerController/Topdown/Player.gd
@@ -1,27 +1,34 @@
 extends CharacterBody3D
 
 
-@export var gravity : Vector3 = Vector3(0, -9.81, 0)
-@export var speed : float = 22
-@export var jump_height : float = 2.4
-var jump_velocity_to_add : float = 0
+@export var speed : float = 10.0
+@export var jump_height : float = 1
+@export var acceleration : float = 100
 
-func get_input() -> Vector3:
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var velocity2 = (input_dir * speed)
-	return Vector3(-velocity2.x, velocity.y, -velocity2.y)
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var gravity_direction = ProjectSettings.get_setting("physics/3d/default_gravity_vector")
 
-func _ready():
-	pass
-
-func _input(event:InputEvent):
-	if is_on_floor() and event.is_action_pressed("Jump"):
-		jump_velocity_to_add = sqrt(2*jump_height*(-gravity.y))
 
 func _physics_process(delta):
-	velocity = get_input()
-	velocity += gravity * delta
-	velocity += Vector3(0, jump_velocity_to_add, 0)
-	jump_velocity_to_add = 0
-	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += (gravity_direction * gravity) * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		var jump_magnitude = sqrt(gravity * 2 * jump_height)
+		velocity = gravity_direction * -jump_magnitude
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = move_toward(velocity.x, direction.x * speed, acceleration * delta)
+		velocity.z = move_toward(velocity.z, direction.z * speed, acceleration * delta)
+	else:
+		velocity.x = move_toward(velocity.x, 0, acceleration * delta)
+		velocity.z = move_toward(velocity.z, 0, acceleration * delta)
+
 	move_and_slide()
