1bf90ea9f8491fb0ab83b6a1e204df3492d3f845
Yee Chien Tang <tychien99@msn.com>
2024-01-19 13:56:52 -0800

    Removed InterpolatedCamera and FollowCamera. Prefer to use Phantom Camera instead

--- addons/FollowCamera/fixed_follow_camera.gd
+++
@@ -1,8 +0,0 @@
-extends Camera3D
-class_name FixedFollowCamera
-
-@export var target : Node3D
-@export var translation : Vector3 = Vector3(-3, 3, 0)
-
-func _process(delta):
-	position = target.position + translation
