3155f3eaadf2f9db3c167ab6595865584ba64647
Yee Chien Tang <tychien99@msn.com>
2024-02-23 16:57:48 -0800

    Created a topdown item placer

---
+++ addons/topdown_item_placer/PlacedItem.gd
@@ -0,0 +1,8 @@
+class_name PlacedItem
+extends Resource
+
+## Scene that will follow the mouse around as it is being placed
+@export var preview_scene : PackedScene
+## Scene that will be sent as a signal when item is placed
+@export var placed_scene : PackedScene
+@export var name : String
