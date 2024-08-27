49c1bb9a6b99676468a5b8e4aaf7cfd91ee048b1
Yee Chien Tang <tychien99@msn.com>
2024-08-26 17:04:03 -0700

    Moved follow target into physics process

--- addons/follow_target/follow_target.gd
+++ addons/follow_target/follow_target.gd
@@ -5,7 +5,7 @@ extends Node3D
 @export var target: Node3D
 @export var localOffset: Vector3
 
-func _process(_delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	var translated = target.transform.translated_local(localOffset)
 	rotation = target.rotation
 	position = translated.origin
