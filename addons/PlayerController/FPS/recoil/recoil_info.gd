48b8d947a220d1c5923353f68ae6ac0db04cb0fc
Yee Chien Tang <tychien99@msn.com>
2024-11-03 17:15:12 -0800

    Add documentation for RecoilInfo

--- addons/PlayerController/FPS/recoil/recoil_info.gd
+++ addons/PlayerController/FPS/recoil/recoil_info.gd
@@ -2,5 +2,7 @@ class_name RecoilInfo
 extends Resource
 
 @export var recoilVector: Vector3 = Vector3(0.5, 0.1, 0)
+## How quickly it snaps towards the target recoil vector
 @export var snappiness: float = 10
+## How quickly we return towards neutral
 @export var returnSpeed: float = 10
