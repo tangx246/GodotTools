3d8574de62838b302047b7b7d6a47992c33cb500
Yee Chien Tang <tychien99@msn.com>
2024-11-08 14:26:53 -0800

    Reorganized addons

--- addons/webrtc_signaling/plugin.gd
+++ addons/webrtc_signaling/plugin.gd
@@ -3,7 +3,7 @@ extends EditorPlugin
 
 func _enter_tree():
 	add_custom_type("PlayerSpawner", "MultiplayerSpawner", preload("game/player_spawner.gd"), preload("icon.svg"))
-	add_autoload_singleton("MultiplayerSceneSwitcher", "res://addons/webrtc_signaling/game/scene_switcher.gd")
+	add_autoload_singleton("MultiplayerSceneSwitcher", preload("game/scene_switcher.gd").new().get_script().get_path())
 
 func _exit_tree():
 	remove_custom_type("PlayerSpawner")
