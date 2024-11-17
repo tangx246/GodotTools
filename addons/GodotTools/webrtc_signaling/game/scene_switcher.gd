6c8bb52f233b0da8bcbb462222a3506c741cdcc9
Yee Chien Tang <tychien99@msn.com>
2024-11-17 10:39:58 -0800

    Updated scene switcher to work around Godot bug

--- addons/GodotTools/webrtc_signaling/game/scene_switcher.gd
+++ addons/GodotTools/webrtc_signaling/game/scene_switcher.gd
@@ -14,12 +14,28 @@ func switch_scenes(new_scene : PackedScene) -> bool:
 	return true
 
 func _switch_scenes(new_scene : PackedScene) -> void:
-	_clear_children()
+	await _clear_children()
 	
 	var instantiated := new_scene.instantiate()
 	gameRoot.add_child(instantiated, true)
 
 func _clear_children():
+	# We have to slowly clear things under MultiplayerSpawners due to some mysterious Godot crash bug
+	for spawner in gameRoot.find_children("", "MultiplayerSpawner", true, false):
+		# Looped items may not be there, apparently. Another Godot bug?
+		if not spawner:
+			continue
+			
+		for child in spawner.get_children():
+			# Same bug as above
+			if not child:
+				continue
+			child.queue_free()
+			await get_tree().process_frame
+		
+		spawner.queue_free()
+		await get_tree().process_frame
+		
 	for child in gameRoot.get_children():
 		gameRoot.remove_child(child)
 		child.queue_free()
