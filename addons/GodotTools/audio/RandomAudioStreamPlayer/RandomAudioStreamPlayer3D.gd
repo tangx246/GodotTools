e176b8d488de117eb0b9066bd9ddf2837940fe82
Ishnubarak <tychien99@msn.com>
2025-06-15 21:00:04 -0700

    Removed RandomAudioStreamPlayer as Godot supports this natively

--- addons/GodotTools/audio/RandomAudioStreamPlayer/RandomAudioStreamPlayer3D.gd
+++
@@ -1,9 +0,0 @@
-class_name RandomAudioStreamPlayer3D
-extends AudioStreamPlayer3D
-
-@export var streams : Array[AudioStream]
-
-func _ready() -> void:
-	stream = streams[randi_range(0, streams.size() - 1)]
-	if autoplay:
-		play()
--- addons/GodotTools/audio/RandomAudioStreamPlayer/plugin.gd
+++
@@ -1,8 +0,0 @@
-@tool
-extends EditorPlugin
-
-func _enter_tree():
-	add_custom_type("RandomAudioStreamPlayer3D", "Node3D", preload("RandomAudioStreamPlayer3D.gd"), preload("icon.svg"))
-
-func _exit_tree():
-	remove_custom_type("RandomAudioStreamPlayer3D")
