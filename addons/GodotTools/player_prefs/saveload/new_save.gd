4f158bd226bb99d76dc21ac9946a48ffc403aa1b
Ishnubarak <tychien99@msn.com>
2025-06-26 22:43:27 -0700

    SaveGames - Fixed bug where non-multiprocess host cannot save games

--- addons/GodotTools/player_prefs/saveload/new_save.gd
+++ addons/GodotTools/player_prefs/saveload/new_save.gd
@@ -9,6 +9,6 @@ func _on_pressed() -> void:
 	_save_rpc.rpc_id(MultiplayerPeer.TARGET_PEER_SERVER, new_save_name.text)
 	get_window().hide()
 
-@rpc("any_peer", "call_remote", "reliable")
+@rpc("any_peer", "call_local", "reliable")
 func _save_rpc(save_name: String) -> void:
 	GameState.save_game(1000000, save_name) # Just make up a large number
