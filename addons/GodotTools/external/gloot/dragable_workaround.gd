5ac5bc5747722fc195f873f457a5df2529211f1d
Ishnubarak <tychien99@msn.com>
2025-06-30 17:10:56 -0700

    Gloot addons - Added a workaround for scene switching while dragging

---
+++ addons/GodotTools/external/gloot/dragable_workaround.gd
@@ -0,0 +1,14 @@
+extends Control
+
+const CtrlDragable = preload("res://addons/gloot/ui/ctrl_dragable.gd")
+
+## Fixes a bug where CtrlDragable is left in an unusable state when scene switches occur
+func _ready() -> void:
+	await get_tree().process_frame
+	
+	get_tree().get_first_node_in_group(MultiplayerSceneSwitcher.GROUP).get_gameroot().child_order_changed.connect(func():
+		get_viewport().gui_cancel_drag()
+		for cl in get_tree().current_scene.find_children("", "CanvasLayer", false, false):
+			push_warning("CanvasLayer found in current_scene root: %s. Usually caused by scene switching while dragging" % cl)
+			cl.queue_free()
+	)
--- addons/GodotTools/external/gloot/plugin.gd
+++ addons/GodotTools/external/gloot/plugin.gd
@@ -3,6 +3,9 @@ extends EditorPlugin
 
 func _enter_tree():
 	add_custom_type("LootTableFiller", "Node3D", preload("loot_table/loot_table_filler.gd"), preload("icon.svg"))
+	var loaded = preload("dragable_workaround.gd")
+	add_autoload_singleton("DragableWorkaround", loaded.resource_path)
 
 func _exit_tree():
 	remove_custom_type("LootTableFiller")
+	remove_autoload_singleton("DragableWorkaround")
